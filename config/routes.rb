46c3bca8b756a5728d7d92a8b4370c7816cb0672
marcosandoval <segundo.marcogmail.com>
2022-03-16 11:01:39 -0600

    Commit until chapter 20-22

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,22 @@ Rails.application.routes.draw do
 
   get "about-us", to: "about#index", as: :about
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
+
+  get "sign-up", to: "registrations#new"
+  post "sign-up", to: "registrations#create"
+  
+  get "sign-in", to: "sessions#new"
+  post "sign-in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
+
   root to: "main#index"
 
 end
